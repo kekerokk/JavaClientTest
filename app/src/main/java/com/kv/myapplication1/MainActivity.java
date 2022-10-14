4cfe790c3a552bb75eea6a29608f2a238c851428
user <chelove4edd@mail.ru>
2022-10-14 15:20:28 +0300

    пытался разобраться с RecycleView. Чето написал но не запускается

--- app/src/main/java/com/kv/myapplication1/MainActivity.java
+++ app/src/main/java/com/kv/myapplication1/MainActivity.java
@@ -1,34 +1,44 @@
 package com.kv.myapplication1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import com.kv.myapplication1.databinding.ActivityMainBinding;
 
 import java.io.IOException;
 import java.net.Socket;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding __binding;
     private Object Socket;
+    private StateAdapter adapter;
+    private List<State> data;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /*__binding=ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(__binding.getRoot());*/
+        RecyclerView recycler = findViewById(R.id.recycler);
+        recycler.setHasFixedSize(true);
+        recycler.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new StateAdapter(this,data);
+        recycler.setAdapter(adapter);
+        adapter.notifyItemInserted(this.data.size()-1);
 
-        __binding.button.setOnClickListener(view -> {
-            try {
-                Repos Connection = new Repos();
-                Connection.sendMessage();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
+    }
 
+    public void add(View view)
+    {
+        EditText edit = this.findViewById(R.id.editText);
+        this.data.add(new State(edit.getText().toString()));
+        edit.setText("");
+        adapter.notifyItemInserted(this.data.size()-1);
     }
 }
---
+++ app/src/main/java/com/kv/myapplication1/State.java
@@ -0,0 +1,21 @@
+package com.kv.myapplication1;
+
+public class State {
+
+    private String name;
+    //private int score;
+
+    public State(String name)
+    {
+        this.name = name;
+        //this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
